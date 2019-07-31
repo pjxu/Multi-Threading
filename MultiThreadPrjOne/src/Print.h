5129daba4c72647608bfa0b0fe7665087ad0ec1c
Jinx <19243068+pjxu@users.noreply.github.com>
2019-07-31 17:14:16 +0800

    Add files via upload

---
+++ MultiThreadPrjOne/src/Print.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <thread>
+#include <string>
+using namespace std;
+class Thread
+{
+public:
+    Thread()
+    {
+        //
+    }
+    virtual ~Thread()
+    {
+
+    }
+    //Print Out Function 1
+    void PrintOne()
+    {
+        printf_s("One\n");
+    }
+    //Print Out Function 2
+    void PrintTwo()
+    {
+        printf_s("Two\n");
+    }
+    //Print Out Function 3
+    void PrintThr()
+    {
+        printf_s("Three\n");
+    }
+};
